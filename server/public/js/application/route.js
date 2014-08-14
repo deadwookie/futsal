c9f04feb7a76b30232fec747e81bcdcf5d368ebc
Dmitry Banasko <dimon.bad@gmail.com>
2014-08-15 00:33:07 +0300

    fix login blink for active sessions

--- server/public/js/application/route.js
+++ server/public/js/application/route.js
@@ -4,5 +4,10 @@ module.exports = Ember.Route.extend({
   model: function() {
     var currentUser = this.get('auth').get('currentUser');
     return currentUser ? this.store.find('player', currentUser.id) : Ember.Object.create();
+  },
+
+  beforeModel: function() {
+    // lets try to _loginActiveSession
+    return this.get('auth').login();
   }
 });
